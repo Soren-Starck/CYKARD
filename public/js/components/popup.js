d5b9ca6a7195d8e409a75e8ad79d4e0aaab64fc6
Søren Starck <soren.starck@etu.umontpellier.fr>
2024-03-28 20:41:09 +0100

    feat: loading spinner on table data fetching

--- public/js/components/popup.js
+++ public/js/components/popup.js
@@ -7,13 +7,13 @@ export class Popup extends ReactiveComponent {
     }
 
     render() {
-    if (this.props.visible === "false") return "";
-    return `
-<div onclick="close" class="w-full h-full backdrop-blur backdrop-brightness-50 absolute top-0 left-0"></div>
+        if (this.props.visible === "false") return "";
+        return `
+    <div onclick="close" class="w-full h-full backdrop-blur backdrop-brightness-50 absolute top-0 left-0"></div>
     <div class="absolute top-1/4 z-50 w-1/2 max-h-1/2 left-1/4 border bg-white shadow-lg rounded-lg p-4">
-    PopupContent example
-    <i onclick="close" class="fas fa-times absolute top-2 right-2 cursor-pointer"></i>
+        PopupContent example
+        <i onclick="close" class="fas fa-times absolute top-2 right-2 cursor-pointer"></i>
     </div>
     `;
-}
+    }
 }
