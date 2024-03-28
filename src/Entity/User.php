26d4916998bcbc0da03c25416446b1abba712357
Hirchyts Daniil <daniil.hirchyts@etu.umontpellier.fr>
2024-03-28 12:09:42 +0100

    git cache cleared

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,14 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
+
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[UniqueEntity(fields: ['login'], message: 'There is already an account with this login')]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+class User
 {
 
     #[ORM\Id]
