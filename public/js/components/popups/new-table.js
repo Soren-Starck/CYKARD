46ecc71e1d51170fe67def378c99626c86d4c2ee
Marius Brt <marius.brouty@gmail.com>
2024-03-30 19:19:58 +0100

    feat: Create and delete table

--- public/js/components/popups/new-table.js
+++ public/js/components/popups/new-table.js
@@ -1,21 +1,19 @@
 import {Popup} from "../popup.js";
 import {API} from "../../api.js";
-import {ColumnStore} from "../../stores/column-store.js";
 
 export class NewTable extends Popup {
     async submit(e) {
         const data = API.formHandler(e)
         const result = await API.create(`/tableau`, data)
         if (!result) return
-        ColumnStore.addTable(result)
-        this.close()
+        window.location.href = `/tableaux/${result.id}`
     }
 
     render() {
         return super.render(`
         <form onsubmit="submit" class="flex flex-col gap-2">
             <label for="title">Titre</label>
-            <input type="text" id="title" name="titretableau" required autofocus>
+            <input type="text" id="title" name="titretableau" value="Mon tableau" required autofocus>
             <button type="submit">Créer</button>
         </form>
         `)
