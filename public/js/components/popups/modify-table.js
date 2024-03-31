1ed427366db74a6d3c5a72a97a274619f034bb79
Søren Starck <soren.starck@etu.umontpellier.fr>
2024-03-31 18:28:23 +0200

    feat : front modify-table gestion des utilisateurs

--- public/js/components/popups/modify-table.js
+++ public/js/components/popups/modify-table.js
@@ -39,11 +39,11 @@ export class ModifyTable extends Popup {
     }
 
     render() {
-        const userList = this.state.me && this.state.me.role === "USER_ADMIN" ? this.state.users.map(user => `
-            <div class="flex justify-between">
-                <p>${user.login}</p>
+        const userList = this.state.me && this.state.me.role !== "USER_ADMIN" ? this.state.users.map(user => `
+            <div class="flex justify-between rounded-md bg-slate-100 px-3 py-1">
+                <p class="font-medium">${user.login}</p>
                 ${user.login === this.state.me.login ? "" : `
-                <i onclick="removeUser" data-login="${user.login}" class="text-red-500 cursor-pointer fa-solid fa-trash"></i>`}
+                <i onclick="removeUser" data-login="${user.login}" class="mt-1 text-red-500 cursor-pointer fa-solid fa-trash"></i>`}
             </div>
         `).join("") : ""
 
@@ -51,10 +51,17 @@ export class ModifyTable extends Popup {
         <form onsubmit="submit" class="flex flex-col gap-2">
             <label for="title">Titre</label>
             <input type="text" id="title" value="${Store.get("table")}" name="titretableau" required autofocus>
+            <label for="user">Utilisateurs</label>
+            <div class="flex relative">
+                <input type="text" id="user" name="user" class="border !rounded-l px-4 py-2 w-full h-9">
+                <button class="absolute right-0 border bg-slate-100 text-neutral-900 !rounded-r flex items-center h-9 px-4 py-2 my-2">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
             ${userList}
             <button type="submit" class="mt-5">Modifier</button>
             ${this.state.me && this.state.me.role !== "USER_ADMIN" ? `
-                <button onclick="leave">Quitter</button>` : ""}
+                <button class="w-full !bg-red-100 !text-red-500 h-9 font-medium rounded-md text-sm" onclick="leave">Quitter</button>` : ""}
         </form>
         `)
     }
