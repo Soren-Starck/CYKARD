5b3cef6f03d21331d540ea380eb4c1048bf68f42
Hirchyts Daniil <daniil.hirchyts@etu.umontpellier.fr>
2024-04-02 17:00:14 +0200

    fix: change role api

--- src/Service/TableauService.php
+++ src/Service/TableauService.php
@@ -56,7 +56,7 @@ class TableauService extends GeneriqueService implements I_TableauService
         $adminRole = $this->tableauRepository->verifyAdminTableauAccess($login, $id);
         if ($adminRole == []) return ['error' => 'Access Denied', 'status' => 403];
         if($user == $login) return ['error' => 'Cannot modify own role', 'status' => 400];
-        $dbResponse = $this->tableauRepository->editUserRoleTableau($id, $role, $login);
+        $dbResponse = $this->tableauRepository->editUserRoleTableau($id, $role, $user);
         if (!$dbResponse) return ['error' => 'Error modifying user role', 'status' => 500];
         return $this->showTableau($login, $id);
     }
