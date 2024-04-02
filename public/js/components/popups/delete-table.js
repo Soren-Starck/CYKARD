8417c57cbb8b98b05e04a9d91fc5b126ee97307d
Marius Brt <marius.brouty@gmail.com>
2024-04-02 14:57:43 +0200

    Update delete-table.js

--- public/js/components/popups/delete-table.js
+++ public/js/components/popups/delete-table.js
@@ -5,10 +5,8 @@ import {Popup} from "../popup.js";
 export class DeleteTable extends Popup {
     submit(e) {
         e.preventDefault()
-        this.setState({loading: true})
         API.remove(`/tableau/${this.props.table_id}/delete`)
         TablesStore.delete(this.props.table_id)
-        this.setState({loading: false})
         this.close()
     }
 
@@ -18,7 +16,7 @@ export class DeleteTable extends Popup {
         return super.render(`
         <form onsubmit="submit" class="flex flex-col gap-2">
             <p>Le tableau '${name}' sera supprimé à jamais.</p>
-            <button type="submit" ${this.state.loading ? "disabled" : ""} class="!bg-red-100 !text-red-500 w-full">
+            <button type="submit" class="!bg-red-100 !text-red-500 w-full">
             <i class="fas fa-trash"></i>
             Supprimer</button>
         </form>
