a09c15675d9bdf11ffd271aed95d8c1bd8a39705
Marius Brt <marius.brouty@gmail.com>
2024-04-02 20:04:54 +0200

    Update card.js

--- public/js/components/table/card.js
+++ public/js/components/table/card.js
@@ -43,9 +43,9 @@ export class Card extends ReactiveComponent {
         return `<div ${canModify ? `onclick="editCard" draggable="true" ondragstart="drag"` : ''} class="hover:opacity-70 hover:!border-blue-600 transition relative bg-white shadow rounded-md border-2 p-2 flex flex-col gap-1 ${canModify ? "hover:cursor-grab active:cursor-grabbing" : ''} group" style="border-color: ${this.state.data.couleurcarte === '#ffffff' ? '#e5e7eb' : this.state.data.couleurcarte}">
             <div class="w-14 h-3 rounded-full absolute top-3 right-4" style="background: ${this.state.data.couleurcarte}"></div>
             <p class="font-bold">${this.state.data.titrecarte}</p>
-            ${matches ? `<img src="${matches}" class="w-full h-36 object-cover rounded-md bg-zinc-300" alt="Image de la carte">` : ""}
+            <p class="bg-neutral-900 text-sm rounded-lg px-2 text-white w-fit">${this.state.data.user_carte_login ?? ""}</p>
+            ${matches ? `<img src="${matches}" class="w-full mt-2 h-36 object-cover rounded-md bg-zinc-300" alt="Image de la carte">` : ""}
             <p>${description}</p>
-            <p class="absolute bottom-3 right-4 bg-neutral-900 rounded-lg px-2 text-white w-fit">${this.state.data.user_carte_login ?? ""}</p>
         </div>
         `;
     }
